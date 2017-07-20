af7ebd07974d8e42ef7c96f0f41875884ac56830
Italo Maia <italo.maia@gmail.com>
2017-07-19 22:47:02 -0300

    gmatch simplified the solution

--- lua/word-count/word-count.lua
+++ lua/word-count/word-count.lua
@@ -1,25 +1,8 @@
 local module = {}
 
-local function inrange(chr)
-  local code = string.byte(chr)
-  return (48 <= code and code <= 57) 
-    or (65 <= code and code <= 90)
-    or (97 <= code and code <= 122)
-end
-
 function module.word_count (text)
-  text = text:lower()
-  local word = ''
   local count = {}
-  
-  for i=1,#text do
-    local chr = text:sub(i, i)
-    if inrange(chr) then word = word .. chr end
-    if chr == ' ' and #word > 0 then
-      count[word], word = (count[word] or 0) + 1, ''
-    end
-  end
-  if #word > 0 then
+  for word in text:lower():gmatch '[%a%d]+' do
     count[word] = (count[word] or 0) + 1
   end
   return count
