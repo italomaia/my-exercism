6c6edf71f93da4e7b27c34b8d5cd9850095f6a9b
Italo Maia <italo.maia@gmail.com>
2023-09-23 18:31:38 +0200

    using type conversion to get char codes; simplified `annotate` a bit

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,7 +1,12 @@
 use std::cmp;
 
-const SPACE: u8 = 32;
-const MINE: u8 = 42;
+// note the solution converts usize to isize; that means we cannot
+// access all possible positions in minefield if minefield is too large
+// and might trigger a indexError for some corner cases
+
+const SPACE: u8 = ' ' as u8;
+const MINE: u8 = '*' as u8;
+const ZERO: u8 = '0' as u8;
 
 fn count_mines(x: isize, y: isize, minefield: &[&str], lines: isize, cols: isize) -> u8 {
   let mut count: u8 = 0;
@@ -20,27 +25,22 @@ fn count_mines(x: isize, y: isize, minefield: &[&str], lines: isize, cols: isize
 }
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-  let mut rs: Vec<String> = Vec::new();
-  let lines = minefield.len() as isize;
-  let cols = match lines.cmp(&0) {
-    cmp::Ordering::Equal => 0,
-    _ => minefield[0].len()
-  } as isize;
+  let size = minefield.len() as isize;
 
-  for x in 0..lines {
-    let row: Vec<u8> = minefield[x as usize].as_bytes().iter().enumerate().map(|(y, field)| {
-      if field == &SPACE {
-        let count = count_mines(x as isize, y as isize, minefield, lines, cols);
-        return match count.cmp(&0) {
-          cmp::Ordering::Equal => SPACE,
-          _ => count + 48
+  minefield.iter().enumerate().map(|(x, row)| {
+    String::from_utf8(
+      row.as_bytes().iter().enumerate().map(move |(y, field)| {
+        if field == &SPACE {
+          let count = count_mines(x as isize, y as isize, minefield, size, row.len() as isize);
+          return match count.cmp(&0) {
+            cmp::Ordering::Equal => SPACE,
+            _ => count + ZERO
+          }
         }
-      }
 
-      MINE
-    }).collect();
-    rs.push(String::from_utf8(row).unwrap());
-  }
-
-  rs
+        MINE
+      })
+      .collect::<Vec<u8>>()
+    ).unwrap()
+  }).collect()
 }
